89520af1d766f81055329dfa23e94bdbc6a21f92
daboogideon <daboogideon@gmail.com>
2020-08-06 14:44:18 -0800

    my examples

---
+++ tabletsapp/src/index.js
@@ -0,0 +1,15 @@
+import  React from 'react'
+import ReactDom from 'react-dom'
+
+
+class message extends React.Component
+{
+
+render(
+  return(
+  <hi>title</hi>)
+  <hi></hi>
+    }
+}
+
+ReactDom.render(<message />, document.getElementById('root'))
---
+++ tabletsapp/src/weil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+
+
+
+  const welcome = React.createElement('<button>',{
+    id ;'footer',
+save
+    className; 'high'
+    save
+  )
+
+
+export default App;
